a94e5130b519f7dde43090a6a4613a6baa2fffcd
ErickKok <99033121@mydavinci.nl>
2016-12-05 08:50:30 +0100

    no message

--- scripts/form.php
+++ scripts/form.php
@@ -2,9 +2,8 @@
 <html>
 <body>
 
-<<?php 
-echo "My first PHP script!";
-?>
+<?php echo "My first PHP script!";?>
+Your email address is: <?php echo $_POST["erick_kok@hotmail.com"]; ?>
 
 </body>
 </html>
